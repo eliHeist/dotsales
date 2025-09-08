ee86f8298a37be16ba119895ad435af1a225dd58
Elijah Triumph <eliyang256@outlook.com>
2025-09-08 06:36:02 +0300

    init

---
+++ dotsales/appsConfig.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+
+app_configs = [
+	{ 'app_name': 'accounts.companies', 'url': 'accounts/companies/', 'namespace': 'companies' },
+
+	{ 'app_name': 'inventory.sales', 'url': 'inventory/sales/', 'namespace': 'sales' },
+
+	{ 'app_name': 'accounts.users', 'url': 'accounts/users/', 'namespace': 'users' },
+
+	{ 'app_name': 'inventory.stock', 'url': 'inventory/stock/', 'namespace': 'stock' },
+
+	{ 'app_name': 'inventory.products', 'url': 'inventory/products/', 'namespace': 'products' },
+
+    # { "app_name": "finances.payments", "url": "finances/payments", "namespace": "payments" },
+]
+
+def getAppUrls():
+    urlpatterns = []
+    for config in app_configs:
+        urlpatterns.append(
+            path(f"{config['url']}", include(f"{config['app_name']}.urls", namespace=config['namespace']))
+        )
+    return urlpatterns
+
+def getAppNames():
+    return [config['app_name'] for config in app_configs]
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from dotsales!")
+
+
+if __name__ == "__main__":
+    main()
