8f90b342655a61cc428dcef6d22ee41da692f15f
Elijah Triumph <eliyang256@outlook.com>
2025-09-22 12:25:40 +0300

    token schemas

--- accounts/users/schemas.py
+++ accounts/users/schemas.py
@@ -6,36 +6,14 @@ from accounts.companies.schemas import BranchModelSchema, CompanyModelSchema
 from .models.User import User
 from .models.UserProfile import UserProfile
 
-class LoginSchema(Schema):
+# Schemas for validation
+class TokenObtainPairIn(Schema):
     email: str
     password: str
 
-class UserProfileSchema(ModelSchema):
-    class Meta:
-        model = UserProfile
-        fields = ["first_name", "middle_name", "last_name", "gender", "phone_1", "phone_2"]
- 
-class UserOutAuthedSchema(ModelSchema):
-    company: CompanyModelSchema
-    accessible_branches: List[BranchModelSchema] = []
-    profile: UserProfileSchema = None
-
-    class Meta:
-        model = User
-        fields = ["id", "email", "username", "company", "accessible_branches", "is_company_admin"]
-
-    @staticmethod
-    def resolve_company(obj):
-        return obj.company
-
-    @staticmethod
-    def resolve_accessible_branches(obj):
-        return obj.accessible_branches.all()
-
-    @staticmethod
-    def resolve_profile(obj):
-        return obj.profile
-
-class LoginResponseSchema(Schema):
+class TokenObtainPairOut(Schema):
     access: str
-    user: UserOutAuthedSchema
+    refresh: str
+
+class TokenRefreshIn(Schema):
+    refresh: str
